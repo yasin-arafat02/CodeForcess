57ba487a8f07578c848c9c2414db0b3a54cf39dc
yasin-arafat02 <arn.shibloo02@gmail.com>
2025-07-05 01:39:35 +0600

    submit

---
+++ 1030-A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,count=0;cin>>n;
+    int arr[n];
+    for (int i = 1; i <= n; i++)cin>>arr[i]; 
+    for (int i = 0; i <= n; i++){
+        if (arr[i]==1)count++;
+    }
+    if (count>0)cout<<"HARD";
+    else cout<<"EASY"; 
+}
